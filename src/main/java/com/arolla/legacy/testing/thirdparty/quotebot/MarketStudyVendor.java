b57b960f7ac7da2dcafe91263eed6e637465fb68
MacReiben <macreiben@gmail.com>
2023-05-01 14:45:10 +0200

    added some random time when publishing.

--- src/main/java/com/arolla/legacy/testing/thirdparty/quotebot/MarketStudyVendor.java
+++ src/main/java/com/arolla/legacy/testing/thirdparty/quotebot/MarketStudyVendor.java
@@ -6,13 +6,28 @@ import javax.swing.JOptionPane;
 
 public class MarketStudyVendor {
 
-	public double averagePrice(String blog) {
-		if (System.getenv("license") == null) {
-			JOptionPane.showMessageDialog(null, "Missing license !",
-					"Stupid license", JOptionPane.WARNING_MESSAGE);
-			throw new RuntimeException("");
-		}
-		return ((double) blog.hashCode() * (new Random()).nextDouble());
-	}
+    public static final int ONE_SECOND = 1000;
+
+    public double averagePrice(String blog) {
+        if (System.getenv("license") == null) {
+            JOptionPane.showMessageDialog(null, "Missing license !",
+                    "Stupid license", JOptionPane.WARNING_MESSAGE);
+            throw new RuntimeException("");
+        }
+
+        SomeComputingThatTakesRandomTime();
+
+        return ((double) blog.hashCode() * (new Random()).nextDouble());
+    }
+
+    private static void SomeComputingThatTakesRandomTime() {
+        var someProcessingTime = new Random().nextInt(10) * ONE_SECOND;
+
+        try {
+            Thread.sleep(someProcessingTime);
+        } catch (InterruptedException e) {
+
+        }
+    }
 
 }
