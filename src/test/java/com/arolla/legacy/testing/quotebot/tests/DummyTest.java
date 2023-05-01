c65d892f00b40b2ec1e4a2e22f3e8e11199cfe8d
MacReiben <macreiben@gmail.com>
2023-05-01 16:23:22 +0200

    some green demo tests.

---
+++ src/test/java/com/arolla/legacy/testing/quotebot/tests/DummyTest.java
@@ -0,0 +1,12 @@
+package com.arolla.legacy.testing.quotebot.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DummyTest {
+    @Test
+    public void some_failing_test() {
+        assertEquals("hello", "hello");
+    }
+}
