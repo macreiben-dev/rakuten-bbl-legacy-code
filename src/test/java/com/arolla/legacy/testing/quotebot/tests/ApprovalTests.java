8395fd8cec40dd6b4567d451862bd536202a1315
MacReiben <macreiben@gmail.com>
2023-05-02 15:08:28 +0200

    approval tests is up. don't forget to set the license env variable.

--- src/test/java/com/arolla/legacy/testing/quotebot/tests/ApprovalTests.java
+++ src/test/java/com/arolla/legacy/testing/quotebot/tests/ApprovalTests.java
@@ -1,2 +1,28 @@
-package com.arolla.legacy.testing.quotebot.tests;public class ApprovalTests {
+package com.arolla.legacy.testing.quotebot.tests;
+
+import com.arolla.legacy.testing.quotebot.Application;
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class ApprovalTests {
+
+    @Test
+    public void Explore() {
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("a\n".getBytes());
+
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(inputStream);
+
+        Application.main(new String[0]);
+
+        var actual = outputStream.toString();
+
+        Approvals.verify(actual);
+    }
 }
