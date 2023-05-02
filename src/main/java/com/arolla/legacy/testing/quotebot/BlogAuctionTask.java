b8c3a918ccc5efc3470fc7dc2d22d2a129ba177d
MacReiben <macreiben@gmail.com>
2023-05-02 17:26:06 +0200

    more readability refactoring.

--- src/main/java/com/arolla/legacy/testing/quotebot/BlogAuctionTask.java
+++ src/main/java/com/arolla/legacy/testing/quotebot/BlogAuctionTask.java
@@ -10,62 +10,56 @@ import com.arolla.legacy.testing.dummies.MarketStudyVendor;
 
 public class BlogAuctionTask {
 
-	public static final String SLOW = "SLOW";
-	public static final String MEDIUM = "MEDIUM";
-	public static final String FAST = "FAST";
-	public static final String ULTRAFAST = "ULTRAFAST";
-	private final MarketStudyVendor marketDataRetriever;
+    public static final String SLOW = "SLOW";
+    public static final String MEDIUM = "MEDIUM";
+    public static final String FAST = "FAST";
+    public static final String ULTRAFAST = "ULTRAFAST";
+    private final MarketStudyVendor marketDataRetriever;
 
-	public BlogAuctionTask() {
-		marketDataRetriever = new MarketStudyVendor();
-	}
+    public BlogAuctionTask() {
+        marketDataRetriever = new MarketStudyVendor();
+    }
 
-	@SuppressWarnings("deprecation")
-	public void PriceAndPublish(String blog, String mode) {
+    @SuppressWarnings("deprecation")
+    public void PriceAndPublish(String blog, String mode) {
 
-		// ====
+        // ====
 
-		System.out.println("BlogAuctionTask.PriceAndPublish - param[blog," + blog + "], param[mode, " + mode + "]");
+        System.out.println("BlogAuctionTask.PriceAndPublish - param[blog," + blog + "], param[mode, " + mode + "]");
 
-		// ====
+        // ====
 
-		double avgPrice = marketDataRetriever.averagePrice(blog);
-		// FIXME should actually be +2 not +1
-		double initialAvgPrice = avgPrice + 1;
-		double timeFactor = 1;
+        final double avgPrice = marketDataRetriever.averagePrice(blog);
+        // FIXME should actually be +2 not +1
+        final double initialAvgPrice = avgPrice + 1;
 
-		if (mode.equals(SLOW)) {
-			timeFactor = 2;
-		}
-		if (mode.equals(MEDIUM)) {
-			timeFactor = 4;
-		}
-		if (mode.equals(FAST)) {
-			timeFactor = 8;
-		}
-		if (mode.equals(ULTRAFAST)) {
-			timeFactor = 13;
-		}
-		
-		double priceToPublish = computeProposal(initialAvgPrice, timeFactor);
+        double timeFactor = switch (mode) {
+            case SLOW -> 2;
+            case MEDIUM -> 4;
+            case FAST -> 8;
+            case ULTRAFAST -> 13;
+            default -> 1;
+        };
 
-		// ======
+        final double priceToPublish = computeProposal(initialAvgPrice, timeFactor);
 
-		System.out.println("BlogAuctionTask.PriceAndPublish - toPublish[" + priceToPublish + "]");
-		//QuotePublisher.INSTANCE.publish(initialAvgPrice);
+        // ======
 
-		// ======
-	}
+        System.out.println("BlogAuctionTask.PriceAndPublish - toPublish[" + priceToPublish + "]");
+        //QuotePublisher.INSTANCE.publish(initialAvgPrice);
 
-	private double computeProposal(double proposal, double timeFactor) {
-		if (proposal % 2 == 0) {
-			return  3.14 * proposal;
-		} else {
-			return 3.15
-					* timeFactor
-					* (new Date().getTime() - new Date(2000, Calendar.JANUARY, 1)
-					.getTime());
-		}
+        // ======
+    }
 
-	}
+    private double computeProposal(double proposal, double timeFactor) {
+        if (proposal % 2 == 0) {
+            return 3.14 * proposal;
+        } else {
+            return 3.15
+                    * timeFactor
+                    * (new Date().getTime() - new Date(2000, Calendar.JANUARY, 1)
+                    .getTime());
+        }
+
+    }
 }
