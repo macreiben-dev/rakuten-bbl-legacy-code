ea6fdbb0a22a8d8aa9800eb7c8e31ff6d016b4d5
MacReiben <macreiben@gmail.com>
2023-05-02 15:57:54 +0200

    adding a dummy market data study vendor.

---
+++ src/main/java/com/arolla/legacy/testing/dummies/MarketStudyVendor.java
@@ -0,0 +1,8 @@
+package com.arolla.legacy.testing.dummies;
+
+public class MarketStudyVendor {
+    public double averagePrice(String blog) {
+        System.out.println("Computing average price for: [" + blog + "]");
+        return 5;
+    }
+}
