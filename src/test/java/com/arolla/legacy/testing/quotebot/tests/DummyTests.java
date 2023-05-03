da2105b6c08854338d8b51e3346cdc27b40cfc4e
MacReiben <macreiben@gmail.com>
2023-05-03 21:23:08 +0200

    dummy test added.

---
+++ src/test/java/com/arolla/legacy/testing/quotebot/tests/DummyTests.java
@@ -0,0 +1,12 @@
+package com.arolla.legacy.testing.quotebot.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DummyTests {
+    @Test
+    public void Dummy() {
+        assertEquals(true, false);
+    }
+}
